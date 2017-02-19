bcd0a957b2d763fb7e5d78bcc17b00492e3d58d9
Mark Benjamin <MarkBenjamin1111111@gmail.com>
2017-02-19 11:11:49 +0000

    add spinner for sort order

--- app/src/main/java/com/halloit/mark/popularmovies/MainActivity.java
+++ app/src/main/java/com/halloit/mark/popularmovies/MainActivity.java
@@ -1,13 +1,17 @@
 package com.halloit.mark.popularmovies;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    public static final String TAG = "MainActivity.java";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,25 +19,38 @@ public class MainActivity extends AppCompatActivity {
         Log.i("MyTheMovieDBApiKeyV4", BuildConfig.THE_MOVIE_DB_API_KEY_V4);
         Log.i("MyTheMovieDBApiKeyV3", BuildConfig.THE_MOVIE_DB_API_KEY_V3);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        Spinner spinner = (Spinner) MenuItemCompat.getActionView(
+                menu.findItem(R.id.menu_spinner));
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter
+                .createFromResource(this, R.array.sort_strings,
+                        android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        if (((String) parent.getItemAtPosition(pos)).equals("Top Rated")) {
+            Log.i(TAG, "selection: Top Rated");
+            // TODO change sort order as necessary
+            // TODO implement logic for reload
         }
-
-        return super.onOptionsItemSelected(item);
+        if (((String) parent.getItemAtPosition(pos)).equals("Popular")) {
+            Log.i(TAG, "selection: Popular");
+            // TODO change sort order as necessary
+            // TODO implement logic for reload
+        }
     }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
 }
